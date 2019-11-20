4660bfd9c6d0b0ec6c2755d1db1db41068778ab4
Steve Astels <sastels@users.noreply.github.com>
2019-11-20 14:32:17 -0500

    Fix no impact confirmation page (#9)

--- routes/confirmationPage/confirmationPage.controller.js
+++ routes/confirmationPage/confirmationPage.controller.js
@@ -4,9 +4,17 @@ const { Schema } = require('./schema.js')
 module.exports = (app, route) => {
   const name = route.name
 
-  route.draw(app)
+  route
+    .draw(app)
     .get((req, res) => {
-      res.render(name, routeUtils.getViewData(req, {}))
+      const data = routeUtils.getViewData(req, {}).data
+      const impactPageDetail = data.impactPage_detail
+      if (impactPageDetail) {
+        if (!Array.isArray(data.impactPage_detail)) {
+          data.impactPage_detail = [data.impactPage_detail]
+        }
+      }
+      res.render(name, routeUtils.getViewData(req, { data }))
     })
     .post(route.applySchema(Schema), route.doRedirect())
 }
